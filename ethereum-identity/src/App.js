1bce8ddac36d2f51ce6ae95524f23d5bf2570f18
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-09-19 23:51:26 +0100

    defining initial var state for the UI

--- ethereum-identity/src/App.js
+++ ethereum-identity/src/App.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 function App() {
 
   const ENDPOINT = "https://ceramic-clay.3boxlabs.com"
+  
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+  const [loaded, setLoaded] = useState(false)
   return (
     <div className="App">
 
