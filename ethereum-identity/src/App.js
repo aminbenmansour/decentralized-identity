9ef1ae698708dcfc1933875da4335bc366e6c242
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-09-19 23:59:46 +0100

    required imports and initializations to read a profile

--- ethereum-identity/src/App.js
+++ ethereum-identity/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+import CeramicClient from "@ceramicnetwork/http-client"
+
+import { IDX } from '@ceramicstudio/idx'
+
 function App() {
 
   const ENDPOINT = "https://ceramic-clay.3boxlabs.com"
@@ -15,6 +19,11 @@ function App() {
     return addresses
   }
   
+  const readProfile = async () => {
+    const [address] = await connect()
+    const ceramic = new CeramicClient(ENDPOINT)
+    const idx = new IDX({ ceramic })
+  }
   return (
     <div className="App">
 
