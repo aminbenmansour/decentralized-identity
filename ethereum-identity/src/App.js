a076cb1c3a44955873ea6dbdeca1039401f5a816
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-09-20 01:06:56 +0100

    implementing function for reading profile

--- ethereum-identity/src/App.js
+++ ethereum-identity/src/App.js
@@ -23,6 +23,21 @@ function App() {
     const [address] = await connect()
     const ceramic = new CeramicClient(ENDPOINT)
     const idx = new IDX({ ceramic })
+
+    try {
+      const data = awaitidx.get(
+        "basicProfile", // scheme
+        `${address}@eip155:1`
+      )
+
+      console.log('data: ', data)
+      if (data.name) setName(data.name)
+      if (data.avatar) setImage(data.avatar)
+      
+    } catch (error) {
+      console.log("error: ", error)
+      setLoaded(true)
+    }
   }
   return (
     <div className="App">
